d129f7885c9590dc75dbdc400baeefbd6694b514
Rihan-saifi46 <kharihan786k@gmail.com>
2025-12-01 22:56:20 +0530

    ad

--- Script.js
+++ Script.js
@@ -25,3 +25,16 @@ const addEventOnElements = function (elements, eventType, callback) {
     elements[i].addEventListener(eventType, callback);
   }
 }
+/**
+ * NAVBAR
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
