4b57a3ec61eaa91d1135026b4c0a0699d19cb50a
Rihan-saifi46 <kharihan786k@gmail.com>
2025-12-01 22:56:03 +0530

    ad

--- Script.js
+++ Script.js
@@ -15,3 +15,13 @@ window.addEventListener("load", function () {
   document.body.classList.add("loaded");
 });
 
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
